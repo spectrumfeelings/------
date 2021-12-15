45fb10a87ccac078a19437483a368463737be5c8
Роман Хитров <cool.fruct@mail.ru>
2021-12-15 22:21:07 +0300

    Добавил комментарий

--- 1.6.2.cpp
+++ 1.6.2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 using namespace std;
-
+//программа является учебной
 int main()
 {
 int a, b;
@@ -9,6 +9,4 @@ cout « "Enter A and B: ";
 cin » a » b;
 cout « "A + B = " « a + b « '\n';
 cout « a*2 « "\n";
-cout << a-b << "\n";
-
 }
